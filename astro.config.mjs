57d17621b246a678efc299419e79bafdf4a00a96
fariidnugraha <farid@jojonomic.com>
2024-01-03 17:44:42 +0700

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import vue from '@astrojs/vue'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,9 @@ export default defineConfig({
         }
       }
     })
-  ]
+  ],
+
+  output: 'server',
+  
+  adapter: netlify(),
 })
